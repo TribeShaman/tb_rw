7054a161368133be347255547f963b787f464f61
w6rstian <quarennc@gmail.com>
2025-01-26 20:58:43 +0100

    Removed unused code

--- Towers/Bullet.gd
+++ Towers/Bullet.gd
@@ -11,6 +11,7 @@ func _physics_process(delta):
 	timeToLive -= delta
 	if timeToLive <= 0:
 		queue_free()
+		
 	var pathSpawnerNode = get_tree().get_root().get_node("Main/PathSpawner")
 	var target = null
 	
@@ -27,12 +28,6 @@ func _physics_process(delta):
 		look_at(target_position)
 		move_and_slide()
 	else:
-		var screen_size = get_viewport().size  # Get the size of the viewport
-
-		# Check if the bullet is outside the screen
-		#if global_position.x < 0 or global_position.y < 0 or global_position.x > screen_size.x or global_position.y > screen_size.y:
-			#queue_free()
-		#else:
 		move_and_slide()
 
 func _on_collision_body_entered(body):
